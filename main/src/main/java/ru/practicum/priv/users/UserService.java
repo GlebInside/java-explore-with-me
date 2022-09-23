9ca3a702370d46fc049f7842f6176c4023d837ba
GlebInside <95642615+GlebInside@users.noreply.github.com>
2022-09-23 12:30:22 +0300

    Add subscription feature

---
+++ main/src/main/java/ru/practicum/priv/users/UserService.java
@@ -0,0 +1,43 @@
+package ru.practicum.priv.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.admin.events.EventMapper;
+import ru.practicum.admin.events.model.State;
+import ru.practicum.admin.users.model.User;
+import ru.practicum.dto.EventShortDto;
+import ru.practicum.priv.events.storage.UserEventRepository;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserEventRepository userEventRepository;
+
+    @Transactional
+    public void subscribe(int subscriberId, int userId) {
+        var model = userRepository.getReferenceById(userId);
+        var subscriber = userRepository.getReferenceById(subscriberId);
+        model.getSubscribers().add(subscriber);
+        userRepository.saveAndFlush(model);
+    }
+
+    public Collection<EventShortDto> getSubscriptionCreatedEvents(int subscriberId) {
+        var users = userRepository.findBySubscribersId(subscriberId);
+
+        return users.stream().flatMap(u -> getActualEvents(u).stream()).collect(Collectors.toList());
+    }
+
+    private Collection<EventShortDto> getActualEvents(User initiator) {
+        return userEventRepository.findByInitiatorAndStateAndEventDateIsAfter(initiator, State.PUBLISHED, LocalDateTime.now()).stream()
+                .map(EventMapper::toShortDto)
+                .collect(Collectors.toList());
+    }
+}
