f6715a0be54259c2e4d5512aeb80592440f140ad
GlebInside <95642615+GlebInside@users.noreply.github.com>
2022-10-02 14:19:28 +0300

    Fixes after review

--- main/src/main/java/ru/practicum/priv/users/UserService.java
+++ main/src/main/java/ru/practicum/priv/users/UserService.java
@@ -3,14 +3,12 @@ package ru.practicum.priv.users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.priv.events.storage.UserEventRepository;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserEventRepository userEventRepository;
 
     @Transactional
     public void subscribe(int subscriberId, int userId) {
